9080a65736df1e8ee0f032ab4943d71f4435c763
michelle <ainun.ilabaca@gmail.com>
2025-04-10 21:36:54 +0700

    komentar anggota not found

--- app/Http/Livewire/Komentar/ListKomentar.php
+++ app/Http/Livewire/Komentar/ListKomentar.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use App\Models\Komentar;
 use App\Models\KataSensor;
 use Illuminate\Support\Facades\Auth;
+
 class ListKomentar extends Component
 {
     public $komentars;
@@ -13,37 +14,38 @@ class ListKomentar extends Component
     public $beritaId;
     public $replyText = [];
     public $kata_sensor = [];
-    public function mount($idBerita) {
+    public function mount($idBerita)
+    {
         $this->beritaId = $idBerita;
         $this->loadComments();
-    foreach($this->komentars as $komentar) {
-        $this->replyText[$komentar->id] = '';
-    }
-    
-
+        foreach ($this->komentars as $komentar) {
+            $this->replyText[$komentar->id] = '';
+        }
     }
     public function render()
     {
-        return view('livewire.komentar.list-komentar',[
+        return view('livewire.komentar.list-komentar', [
             'total_komentar' => $this->komentars->count(),
-        ] );
+        ]);
     }
     public function loadComments()
     {
         $this->komentars = Komentar::where('id_berita', $this->beritaId)
             ->whereNull('parent')
-            ->with(['anggota','childs'])
+            ->with(['anggota' => function ($query) {
+                $query->withTrashed();
+            }, 'childs'])
             ->orderBy('created_at', 'desc')
             ->get();
-        $katasensor= KataSensor::pluck('kata');
-        foreach($this->komentars as $komentar) {
+        $katasensor = KataSensor::pluck('kata');
+        foreach ($this->komentars as $komentar) {
             $komentar->komentar = $this->censorWords($komentar->komentar, $katasensor);
-            
-            foreach($komentar->childs as $child) {
+
+            foreach ($komentar->childs as $child) {
                 $child->komentar = $this->censorWords($child->komentar, $katasensor);
             }
         }
-        
+
         $this->total_komentar = $this->komentars->count();
     }
 
@@ -56,12 +58,12 @@ class ListKomentar extends Component
         $new->parent = null;
         $new->komentar = $this->commentText;
         $new->save();
-      
+
 
         $this->reset('commentText');
-        
+
         $this->loadComments();
-        
+
         session()->flash('message', 'Comment added successfully!');
     }
     public function createReply($beritaId, $parentId)
@@ -72,30 +74,30 @@ class ListKomentar extends Component
         //     'replyText.'.$parentId.'.required' => 'The reply cannot be empty.',
         //     'replyText.'.$parentId.'.min' => 'The reply must be at least 3 characters.',
         // ]);
- 
+
         $new = new Komentar();
         $new->id_berita = $beritaId;
         $new->id_anggota = Auth::user()->id;
         $new->parent = $parentId;
         $new->komentar = $this->replyText[$parentId];
         $new->save();
-      
+
         $this->replyText[$parentId] = '';
-        
+
         $this->loadComments();
 
         $this->emit('refreshComments');
-        
+
         session()->flash('message', 'Reply added successfully!');
-        
+
         $this->dispatchBrowserEvent('replySubmitted', ['parentId' => $parentId]);
     }
-    private function censorWords($text, $sensorWords) 
-{
-    foreach($sensorWords as $word) {
-        $stars = str_repeat('*', strlen($word));
-        $text = str_ireplace($word, $stars, $text);
+    private function censorWords($text, $sensorWords)
+    {
+        foreach ($sensorWords as $word) {
+            $stars = str_repeat('*', strlen($word));
+            $text = str_ireplace($word, $stars, $text);
+        }
+        return $text;
     }
-    return $text;
-}
 }
